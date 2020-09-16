e1c8e016286e8481eeeaf8584e23d0188e715e92
SachalaH <59394760+SachalaH@users.noreply.github.com>
2020-09-16 12:03:20 +0530

    Functionality to add and delete todos

---
+++ app.js
@@ -0,0 +1,29 @@
+const addForm = document.querySelector(".add");
+const todoList = document.querySelector(".todos");
+
+const generateTemplate = (todo) => {
+  const html = `
+    <li 
+        class="list-group-item d-flex justify-content-between align-items-center">
+          <span>${todo}</span>
+          <i class="far fa-trash-alt delete"></i>
+    </li>
+    `;
+
+  todoList.innerHTML += html;
+};
+
+addForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const todo = addForm["add"].value.trim();
+  if (todo.length) {
+    generateTemplate(todo);
+    addForm.reset();
+  }
+});
+
+todoList.addEventListener("click", (e) => {
+  if (e.target.classList.contains("delete")) {
+    e.target.parentElement.remove();
+  }
+});
